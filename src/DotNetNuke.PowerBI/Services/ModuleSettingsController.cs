ffff0e6b9adb5dc0791aeeb3d2e82d6cbb570e3d
Aarón Suárez <16966282+aaronsglz@users.noreply.github.com>
2023-03-31 15:07:13 +0100

    fix: refresh content items where workspace is selected

---
+++ src/DotNetNuke.PowerBI/Services/ModuleSettingsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using DotNetNuke.Web.Api;
+using System.Web.Http;
+using DotNetNuke.Security;
+using DotNetNuke.PowerBI.Models;
+using System.Collections.Generic;
+
+namespace DotNetNuke.PowerBI.Services
+{
+    [SupportedModules("PBIEmbedded,DotNetNuke.PowerBI.ListView,DotNetNuke.PowerBI.ContentView,DotNetNuke.PowerBI.CalendarView")]
+    [DnnModuleAuthorize(AccessLevel = SecurityAccessLevel.View)]
+    public class ModuleSettingsController : DnnApiController
+    {
+        [HttpGet]
+        [DnnAuthorize]
+        public HttpResponseMessage GetContentItemsByGroup(string groupId)
+        {
+            try
+            {
+                EmbedService embedService = new EmbedService(ActiveModule.PortalID, ActiveModule.TabModuleID, groupId);
+
+                PowerBIListView contentItems = embedService.GetContentListAsync(PortalSettings.UserId).Result;
+                if (contentItems != null)
+                {
+                    // Remove other culture contents
+                    contentItems = contentItems.RemoveOtherCultureItems();
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    contentItems
+                });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError);
+            }
+        }
+
+    }
+}
---
+++ src/DotNetNuke.PowerBI/scripts/module-settings.js
@@ -0,0 +1,63 @@
+﻿var app = app || {};
+
+app.settings = function (context) {
+    var that = this;
+    this.service = {
+        path: 'Bookmarks',
+        controller: 'ModuleSettings',
+        framework: $.ServicesFramework(context.ModuleId)
+    };
+    this.service.baseUrl = that.service.framework.getServiceRoot(that.service.path);
+    this.$workspaces;
+
+    this.refreshContentItems = function (groupId) {
+        let params = {
+            groupId: groupId,
+        };
+
+        $.ajax({
+            url: that.service.baseUrl + that.service.controller + '/GetContentItemsByGroup',
+            type: 'GET',
+            async: true,
+            data: params,
+            dataType: '',
+            contentType: '',
+            headers: {
+                'PortalId': context.PortalId,
+                'ModuleId': context.ModuleId,
+                'TabId': context.TabId,
+                'RequestVerificationToken': $.ServicesFramework().getAntiForgeryValue(),
+            }
+        }).done(function (data) {
+            if(data.contentItems != null) {
+                var $contentItems = $('#ContentItemId');
+                $contentItems.empty();
+                $contentItems.append($('<option>', { value: '', text: 'Choose one...' }));
+
+                $.each(data.contentItems.Dashboards, function (i, item) {
+                    $contentItems.append($('<option>', { 
+                        text: 'Dashboard - ' + item.displayName,
+                        value: 'D_' + item.id,
+                    }));
+                });
+                
+                $.each(data.contentItems.Reports, function (i, item) {
+                    $contentItems.append($('<option>', { 
+                        text: 'Reports - ' + item.name,
+                        value: 'R_' + item.id,
+                    }));
+                });
+            }
+        }).fail(function (error, exception) {
+            console.error(error);
+        });
+    };
+
+    this.init = function () {
+        that.workspaces = $('#SettingsGroupId');
+
+        that.workspaces.on('change', function () {
+            that.refreshContentItems(that.workspaces.val());
+        });
+    };
+};
